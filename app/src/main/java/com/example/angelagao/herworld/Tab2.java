6e63ff1abd28584315ac4c04f89aedf184829c12
Angela Gao <angelagao@AngelatekiMacBook-Pro.local>
2016-11-07 16:15:10 -0800

    init commit

---
+++ app/src/main/java/com/example/angelagao/herworld/Tab2.java
@@ -0,0 +1,20 @@
+package com.example.angelagao.herworld;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by angelagao on 11/7/16.
+ */
+public class Tab2 extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
